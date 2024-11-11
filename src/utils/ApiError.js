38f6882b42b09a7e58e8c2d4e7e7e6cdd07de376
wasim haider <wasimhaider14379@gmail.com>
2024-11-11 15:24:44 +0500

    util code discussion

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class APIError extends Error {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+
+        if(stack) {
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constractor)
+        } 
+    }
+}
+
+export {APIError}
